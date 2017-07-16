2fc17de68d7e5379beb19bc3922f6fc0f51ef315
wuzhao <liwuzhao97@gmail.com>
2017-07-16 20:39:45 +0800

    spec test

---
+++ app/models/parking.rb
@@ -0,0 +1,18 @@
+class Parking < ApplicationRecord
+  validates_presence_of :parking_type, :start_at
+  validates_inclusion_of :parking_type, :in => ["guest", "short-term", "long-term"]
+
+  validate :validate_end_at_with_amount
+
+  #自订了一个检查规则是 end_at 和 amount 需要一起填
+  def validate_end_at_with_amount
+    if ( end_at.present? && amount.blank?)
+      errors.add(:amount, "有结束时间就必须有金额")
+    end
+
+    if (end_at.blank? && amount.present?)
+      errors.add(:end_at, "有金额就必须有结束时间")
+    end
+  end
+
+end
