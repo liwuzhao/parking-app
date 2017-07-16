154509a906aa1adc6c2d54c32e4c8c4e47ede36d
wuzhao <liwuzhao97@gmail.com>
2017-07-17 00:06:26 +0800

    parking-app

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -4,7 +4,15 @@ class ParkingsController < ApplicationController
   end
 
   def create
-    @parking = Parking.new( :parking_type => "guest", :start_at => Time.now)
+    @parking = Parking.new(:start_at => Time.now)
+
+    if current_user
+      @parking.parking_type = params[:parking][:parking_type]
+      @parking.user = current_user
+    else
+      @parking.parking_type = "guest"
+    end
+
     @parking.save!
 
     redirect_to parking_path(@parking)
--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,6 +2,8 @@ class Parking < ApplicationRecord
   validates_presence_of :parking_type, :start_at
   validates_inclusion_of :parking_type, :in => ["guest", "short-term", "long-term"]
 
+  belongs_to :user, :optional => true
+
   validate :validate_end_at_with_amount
 
   #自订了一个检查规则是 end_at 和 amount 需要一起填
@@ -22,12 +24,41 @@ class Parking < ApplicationRecord
 
   def calculate_amount
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
-      if duration <= 60
-        self.amount = 200
-      else
-        self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
+      if self.user.blank?
+        self.amount = calculate_guest_term_amount  # 一般费率
+      elsif self.parking_type == "long-term"
+          self.amount = calculate_long_term_amount # 长期费率
+      elsif self.parking_type == "short-term"
+        self.amount = calculate_short_term_amount  # 短期费率
       end
     end
   end
+
+  def calculate_guest_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
+    end
+  end
+
+  def calculate_short_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 50
+    end
+  end
+
+  def calculate_long_term_amount
+    if duration > 360
+      self.amount = 1600
+    else
+      self.amount = 1200
+    end
+  end
+
+
+
 
 end
